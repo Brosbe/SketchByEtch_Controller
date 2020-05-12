62ea0ef93a2098b799d210f16bba9d58e8f90b55
Brosbe <brosbe@protonmail.com>
2020-05-12 14:32:45 +0200

    Update code for new controller style and update connection handling

--- etchASketch/Communicator.cs
+++ etchASketch/Communicator.cs
@@ -3,6 +3,7 @@ using System.Windows.Forms;
 using System.IO.Ports;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace etchASketch
 {
@@ -14,25 +15,16 @@ namespace etchASketch
         private const int MOUSEEVENTF_LEFTDOWN = 0x02;
         private const int MOUSEEVENTF_LEFTUP = 0x04;
 
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
 
         private SerialPort port;
 
-        private bool _IsMouseDown { get; set; }
-        private bool _Loop { get; set; }
         private bool _EtchMode { get; set; }
         private bool _HasConnected { get; set; }
+        private bool _IsHandling { get; set; }
+        private bool _Loop { get; set; }
 
-        public bool IsMouseDown
-        {
-            get { return _IsMouseDown; }
-            private set { _IsMouseDown = value; }
-        }
-
-        public bool Loop
-        {
-            get { return _Loop; }
-            private set { _Loop = value; }
-        }
 
         public bool EtchMode
         {
@@ -46,11 +38,24 @@ namespace etchASketch
             private set { _HasConnected = value; }
         }
 
+        public bool IsHandling
+        {
+            get { return _IsHandling; }
+            private set { _IsHandling = value; }
+        }
+
+        public bool Loop
+        {
+            get { return _Loop; }
+            private set { _Loop = value; }
+        }
+
 
         public Communicator()
         {
             port = new SerialPort();
             Loop = true;
+            port.DataReceived += HandleReceivedDate;
         }
 
         public void serial()
@@ -58,90 +63,92 @@ namespace etchASketch
 
             try
             {
-                Point prevVal = new Point(0, 0);
-                Point cursorpoint = new Point(0, 0);
                 while (Loop)
                 {
-
-                    port.WriteLine(".");
-                    var serialInfo = port.ReadLine();
-                    serialInfo = serialInfo.Substring(0, serialInfo.Length - 1);
-                    string[] split = serialInfo.Split('|');
-
-                    if (split[2] == "1")
+                    if (!IsHandling)
                     {
-                        EtchMode = !EtchMode;
-                        if (IsMouseDown)
-                        {
-                            ToggleMouse();
-                        }
-                    }
-
-                    if (EtchMode)
-                    {
-                        if (split[1] == "1")
-                        {
-                            ToggleMouse();
-                        }
-                        var val = split[0].ToPoint();
-
-                        val.X += 449;
-                        val.Y += 42;
-                        if (val.X != prevVal.X || val.Y != prevVal.X)
-                        {
-                            cursorpoint.X = val.X;
-                            cursorpoint.Y = val.Y;
-                            Cursor.Position = cursorpoint;
-                            prevVal.X = val.X;
-                            prevVal.Y = val.Y;
-                        }
+                        port.WriteLine(".");
+                        Thread.Sleep(6);
                     }
                 }
-
-
             }
             catch (Exception)
             {
-                Loop = false;
                 HasConnected = false;
-                EtchMode = false;
-                if (IsMouseDown)
-                {
-                    ToggleMouse();
-                }
                 MessageBox.Show("Somthing went wrong while trying to communicate with the controller", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             port.Close();
         }
 
-        private void ToggleMouse()
-        {
-            IsMouseDown = !IsMouseDown;
-            if (IsMouseDown)
-            {
-                mouse_event(MOUSEEVENTF_LEFTDOWN, Cursor.Position.X, Cursor.Position.Y, 0, 0);
-                return;
-            }
-            mouse_event(MOUSEEVENTF_LEFTUP, Cursor.Position.X, Cursor.Position.Y, 0, 0);
-
-        }
-
-        public void EndConnection()
-        {
-            Loop = false;
-        }
-
         public void SetPort(string portString)
         {
-            port.BaudRate = 9600;
+            port.BaudRate = 19200;
             port.PortName = portString;
             port.Open();
-            port.WriteLine("-");
-            if (port.ReadLine() != "?\r")
+            try
             {
+                port.WriteLine("-");
+                Thread.Sleep(50);
+                if (!HasConnected)
+                {
+                    port.Close();
+                    throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        private void HandleReceivedDate(object sender, EventArgs e)
+        {
+            IsHandling = true;
+            if (!HasConnected)
+            {
+                if (port.ReadLine() == "?\r")
+                {
+                    HasConnected = true;
+                    IsHandling = false;
+                    return;
+                }
+                IsHandling = false;
                 throw new Exception();
             }
-            HasConnected = true;
+
+            Point prevVal = new Point(0, 0);
+            Point cursorpoint = new Point(0, 0);
+            var serialInfo = port.ReadLine();
+            serialInfo = serialInfo.Substring(0, serialInfo.Length - 1);
+            string[] split = serialInfo.Split('|');
+
+            EtchMode = split[1] == "1";
+
+            if (EtchMode)
+            {
+                var val = split[0].ToPoint();
+
+                val.Y = 1080 - val.Y;
+
+                val.X += 420;
+                if (val.X != prevVal.X || val.Y != prevVal.X)
+                {
+                    SetCursorPos(val.X, val.Y);
+                    //cursorpoint.X = val.X;
+                    //cursorpoint.Y = val.Y;
+                    //Cursor.Position = cursorpoint;
+                    //prevVal.X = val.X;
+                    //prevVal.Y = val.Y;
+                }
+            }
+            IsHandling = false;
+        }
+
+        public void End()
+        {
+            Loop = false;
         }
     }
 }
