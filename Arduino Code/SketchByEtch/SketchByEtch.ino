1f8638c9ec3660d7a7f984ba3bdc3219de69e56a
Brosbe <brosbe@protonmail.com>
2023-03-29 00:11:39 +0200

    Update arduino code adn prepare it for overhaul

--- Arduino Code/SketchByEtch/SketchByEtch.ino
+++ Arduino Code/SketchByEtch/SketchByEtch.ino
@@ -2,11 +2,13 @@
 #include <ResponsiveAnalogRead.h>
 
 char value[1];
-int prevVal = 0;
-int val;
-bool prevPress1 = LOW;
-bool prevPress2 = LOW;
+char serialDelay[2];
+int delayLength = 0;
+int newDelayLength = 0;
 bool curPress;
+//senddata is used to loop sending serialdata with intervals equal to the delayLength.
+//delays will be done using the millis command as this will allow the arduino to still receive serial data to stop the dataflood.
+bool sendData = false;
 ResponsiveAnalogRead analog1(A0, true);
 ResponsiveAnalogRead analog2(A1, true);
 String sender;
@@ -17,35 +19,54 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(3, INPUT);
   Serial.begin(19200);
+  analogReadResolution(12);
+  analog1.setAnalogResolution(4096);
+  analog2.setAnalogResolution(4096);
   //You can remove the analogReadResolution line if you want or change the number to something else.
   //It is simply here to ecord any analog values in a higher resolution so cursor positioning can become more accurate.
-  analogReadResolution(12);
 }
 
 void loop() {
 
-  while(Serial.available() > 0)
+  while(Serial.available())
   {
-      Serial.readBytes(value, 1);
-  if( value[0] == '.')
-  {
-    analog1.update();
-    analog2.update();
-    sender = String((int)(analog1.getValue()), DEC);
-    sender += ",";
-    part = String((int)(analog2.getValue()), DEC);
-    sender += part;
-    sender += "|";
-    curPress = digitalRead(3);
-    sender += String((curPress && prevPress1 != curPress), DEC);
+    Serial.readBytes(value, 1);
+    switch (value[0])
+    {
+    case '.':
+      analog1.update();
+      analog2.update();
+      sender = String((int)(analog1.getValue()), DEC);
+      sender += ",";
+      part = String((int)(analog2.getValue()), DEC);
+      sender += part;
+      sender += "|";
+      sender += String((digitalRead(3)), DEC);
 
-    Serial.println(sender);
-    Serial.flush();
-  }
-  if(value[0] == '-')
-  {
-    Serial.println('?');
-  }
-  }
+      Serial.println(sender);
+      Serial.flush();
+      break;
 
-}
+    case '-':
+      Serial.println('?');
+      Serial.flush();
+      break;
+
+    case 'd':
+      newDelayLength = 0;
+      Serial.readBytes(serialDelay, 2);
+      for (byte i = 0; i < 2; i++)
+      {
+        newDelayLength <<= 8;
+        newDelayLength += serialDelay[i];
+      }
+      delayLength = newDelayLength;
+
+      break;
+
+    default:
+      Serial.flush();
+      break;
+    }
+  }
+} 
