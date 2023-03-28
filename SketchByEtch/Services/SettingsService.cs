7c34ea7456ab36f98b37a839410d098a4411f4bb
Brosbe <brosbe@protonmail.com>
2023-03-29 00:19:15 +0200

    add some settings stuff in the settingsservice

--- SketchByEtch/Services/SettingsService.cs
+++ SketchByEtch/Services/SettingsService.cs
@@ -8,5 +8,15 @@ namespace SketchByEtch.Services
 {
     public class SettingsService
     {
+        public Settings GetSettings()
+        {
+            return null;
+        }
+
+        public bool SaveSettings(Settings settings)
+        {
+            return false;
+        }
+
     }
 }
